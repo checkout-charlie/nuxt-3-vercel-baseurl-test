35d0d80038a778a42aea7445edffb0b55e6ddb24
Johannes <hannes@lautr.com>
2024-04-16 14:06:49 +0200

    wip

---
+++ app.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const counter = ref(0)
+</script>
+<template>
+  <div>
+    counter {{ counter }}
+    <button @click="counter++">
+      +
+    </button>
+  </div>
+</template>
